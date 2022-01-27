67bde42c2d3378c2df129bbc77d6c652a36c15b0
isdiscodead <isdiscodead@korea.ac.kr>
2022-01-27 23:07:29 +0900

    BOJ #10952

---
+++ BOJ_step/05_while_clause.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a, b;
+    // 01. 두 정수 A와 B를 입력받은 다음, A+B를 출력
+    while ( true ) {
+        cin >> a >> b;
+        if ( a==0 && b==0 ) {
+            break;
+        } 
+        cout << a+b << "\n";
+    }
+    
+    return 0;
+}
