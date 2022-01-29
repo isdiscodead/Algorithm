dfb0fc775dac5bb9044e3f56682d0575f2f2fc45
isdiscodead <isdiscodead@korea.ac.kr>
2022-01-29 23:58:46 +0900

    0129

--- BOJ_step/05_while_clause.cpp
+++ BOJ_step/05_while_clause.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(void) {
     int a, b;
+    /*
     // 01. 두 정수 A와 B를 입력받은 다음, A+B를 출력
     while ( true ) {
         cin >> a >> b;
@@ -21,6 +22,24 @@ int main(void) {
         cin >> b;
         cout << a+b << "\n";
     }
+    */
+
+    // 03. 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다. 
+    // 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다
+    // N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
+    cin >> a;
+    cout << a;
+    b = 100;
+    int cycle = 0;
+    while ( b != a ) {
+        b = a/10 + a%10;
+        if ( a < 10 ) {
+            b = 0 + a;
+        }
+        b = (a%10)*10 + b%10;
+        cycle++;
+    }
+    cout << cycle;
     
     return 0;
 }
