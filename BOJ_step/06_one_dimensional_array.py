6a140f98cd752735ee4b06deb19a45b4c9e4ca24
isdiscodead <hyhy1274@naver.com>
2022-02-03 23:58:45 +0900

    #10818

--- BOJ_step/06_one_dimensional_array.py
+++ BOJ_step/06_one_dimensional_array.py
@@ -1,18 +1,17 @@
 # 10818 
 size = int(input())
-
-# num = list(((map(int, input().split())))) 로 줄일 수 있음
 arr = input().split()
-for num in arr :
-    num = int(num)
-    
+
+for i in range(len(arr)) :
+  arr[i] = int(arr[i])
+
 min = arr[0]
 max = arr[0]
 
 for num in arr :
-    if num < min :
-        min = num
-    if num > max :
-        max = num
+  if num <= min :
+    min = num
+  if num >= max :
+    max = num
 
 print(min, max)
