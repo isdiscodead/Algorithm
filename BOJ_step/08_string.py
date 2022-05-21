6bc67312012592a37f83e71ba5b5cc2d98eba3eb
isdiscodead <isdiscodead@korea.ac.kr>
2022-05-21 23:51:19 +0900

    11720 숫자의 합

--- BOJ_step/08_string.py
+++ BOJ_step/08_string.py
@@ -1,3 +1,11 @@
 # 11654 아스키 코드
-code = int(input())
+code = input()
 print( ord(code) )
+
+
+# 11720 숫자의 합
+size = int(input())
+sentence = input()
+total = 0
+for i in range(size):
+    total += int(sentence[i])
