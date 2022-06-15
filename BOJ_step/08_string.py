2f4a77f1b8dada91c2a694927e8ae1378658d3a9
isdiscodead <isdiscodead@korea.ac.kr>
2022-06-16 02:11:13 +0900

    BOJ #10809 알파벳 찾기

--- BOJ_step/08_string.py
+++ BOJ_step/08_string.py
@@ -1,4 +1,7 @@
 # 11654 아스키 코드
+from gettext import find
+
+
 code = input()
 print( ord(code) )
 
@@ -9,3 +12,15 @@ sentence = input()
 total = 0
 for i in range(size):
     total += int(sentence[i])
+print(total)
+
+
+# 10809 알파벳 찾기
+sentence = input()
+found = 0
+alpha = ord('a')
+
+while ( alpha < ord('z') ) :
+    found = sentence.find( chr(alpha) )
+    alpha += 1;
+    print(found, end=" ")
