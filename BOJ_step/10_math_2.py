ff3174940df888d9bb9de5c421ab28cf7dec01e2
isdiscodead <isdiscodead@korea.ac.kr>
2022-10-07 17:15:48 +0900

    #1928

--- BOJ_step/10_math_2.py
+++ BOJ_step/10_math_2.py
@@ -1,4 +1,7 @@
 # 11653 소인수분해 
+from cmath import sqrt
+
+
 n = int(input())
 x = 2;
 
@@ -8,3 +11,32 @@ while ( n > 1 ) :
         print(x)
     else :
         x += 1
+
+
+# 1978 소수 찾기
+# 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력
+from math import sqrt
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+def isPrime(num):
+    if num < 2 :
+        return False
+    if num == 2 :
+        return True
+
+    i = 2
+    while(i < num):
+        if num % i == 0 :
+            return False
+        i += 1
+        
+    return True
+
+count = 0
+for i in range(n):
+    if isPrime(nums[i]) :
+        count += 1
+
+print(count)
