9e4b0bae19b9a10b69e9ee770d4b4d8b382c7a34
isdiscodead <isdiscodead@korea.ac.kr>
2023-03-25 19:16:57 +0900

    ㅁㅓ쓱이보다 키 큰 사람

--- jecobe-js/day02.js
+++ jecobe-js/day02.js
@@ -17,3 +17,27 @@ function solution(numbers) {
 function solution(numbers) {
     return numbers.reduce((acc, cur) => acc + cur) / numbers.length
 }
+
+
+
+/*
+머쓱이보다 키 큰 사람
+머쓱이는 학교에서 키 순으로 줄을 설 때 몇 번째로 서야 하는지 궁금해졌습니다. 
+머쓱이네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때, 머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요.
+*/
+
+// 생각하기 쉬운 풀이 
+function solution(array, height) {
+    var cnt = 0;
+    for ( var i=0 ; i < array.length ; i++ ) {
+        if ( array[i] > height ) {
+            cnt ++;
+        }
+    }
+    return cnt;
+}
+
+// 빠른 풀이 : filter() 함수를 사용해서 키 큰 사람의 배열 생성
+function solution(array, height) {
+    return array.filter(item => item > height).length;
+}
