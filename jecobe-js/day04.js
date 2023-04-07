c7884f2351f48448a2136bfc5e3d7296209af5c5
isdiscodead <isdiscodead@korea.ac.kr>
2023-04-07 20:26:16 +0900

    day5 start

--- jecobe-js/day04.js
+++ jecobe-js/day04.js
@@ -85,4 +85,54 @@ function solution(age) {
       .split("")
       .map((v) => "abcdefghij"[v])
       .join("");
-  }
+}
+
+
+/*
+369 게임
+머쓱이는 친구들과 369게임을 하고 있습니다. 369게임은 1부터 숫자를 하나씩 대며 3, 6, 9가 들어가는 숫자는 숫자 대신 3, 6, 9의 개수만큼 박수를 치는 게임입니다. 머쓱이가 말해야하는 숫자 order
+가 매개변수로 주어질 때, 머쓱이가 쳐야할 박수 횟수를 return 하도록 solution 함수를 완성해보세요.
+ */
+
+// 생각하기 쉬운 풀이
+function solution(order) {
+    var answer = 0;
+		order = order.toString()
+    
+    for ( var i=0 ; i < order.length ; i++ ) {
+        if ( order[i] == 3 || order[i] == 6 || order[i] == 9  ) {
+            answer ++;
+        }
+    }
+    return answer;
+}
+
+// 빠른 풀이 : filter 활용
+function solution(order) {
+    return order.toString().split('').filter(n => ['3', '6', '9'].includes(n)).length;
+}
+
+// 빠른 풀이 : 정규식 + split 활용
+function solution(order) {
+    return (''+order).split(/[369]/).length-1;
+}
+
+
+
+/*
+중복된 문자 제거
+문자열 my_string이 매개변수로 주어집니다. my_string에서 중복된 문자를 제거하고 하나의 문자만 남긴 문자열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+// 생각하기 쉬운 풀이
+function solution(my_string) {
+    var answer = '';
+    var set = [];
+    for ( var i=0 ; i < my_string.length ; i++ ) {
+        if ( ! set.includes(my_string[i]) ) {
+            set.push(my_string[i]);
+            answer += my_string[i];
+        } 
+    }
+    return answer;
+}
