f986b5f7bc2530047c6084d08bdb9edadc9630fa
isdiscodead <isdiscodead@korea.ac.kr>
2023-04-10 20:29:14 +0900

    k의 개수(1)

--- jecobe-js/day05.js
+++ jecobe-js/day05.js
@@ -18,3 +18,37 @@ function solution(n) {
         }
     }
 }
+
+
+/*
+k의 개수
+1부터 13까지의 수에서, 1은 1, 10, 11, 12, 13 이렇게 총 6번 등장합니다. 
+정수 i, j, k가 매개변수로 주어질 때, i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
+*/
+
+// 생각하기 쉬운 풀이
+function solution(i, j, k) {
+    var answer = 0;
+
+    for(let num=i; num<=j ; num++){
+        //num의 길이가 한자리면 -> 그냥 카운트
+
+            if(num == k){
+                console.log(num)
+                answer++;
+                console.log(answer)
+            }
+
+        //num의 길이가 두자리 이상이면 -> 쪼개서 카운트
+        if(num.toString().length >=2){
+         arr = num.toString().split("")
+            arr.forEach((num)=>{
+                if(num == k){
+                    answer ++
+                }
+            })
+        }
+    }
+
+    return answer;
+}
