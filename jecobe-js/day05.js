bcef92460042fd1cfd898f3d9f0f9df1e138792f
isdiscodead <isdiscodead@korea.ac.kr>
2023-04-12 20:30:44 +0900

    -

--- jecobe-js/day05.js
+++ jecobe-js/day05.js
@@ -62,3 +62,11 @@ function solution(i, j, k) {
 
     return a.split(k).length-1;
 }
+
+
+/*
+가까운 수
+정수 배열 array와 정수 n이 매개변수로 주어질 때, array에 들어있는 정수 중 n과 가장 가까운 수를 return 하도록 solution 함수를 완성해주세요.
+*/
+
+// 생각하기 쉬운 풀이
