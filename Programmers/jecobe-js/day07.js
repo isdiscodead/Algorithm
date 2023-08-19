a4fd204707163dcb58bc5e7461bb947f7c950db1
isdiscodead <isdiscodead@korea.ac.kr>
2023-08-19 16:02:26 +0900

    등수 매기기

--- Programmers/jecobe-js/day07.js
+++ Programmers/jecobe-js/day07.js
@@ -8,3 +8,10 @@ function solution(score) {
     return avg.map(v => sorted.indexOf(v) + 1);
 }
 // 빠른 풀이
+function solution(score) {
+    return score.map((el) => {
+        return (
+        score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+        );
+    });
+}
