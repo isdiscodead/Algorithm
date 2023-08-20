ae1f8adc548625740b1df05ed35638a5436cfeef
isdiscodead <isdiscodead@korea.ac.kr>
2023-08-20 16:03:44 +0900

    저주의 숫자 3

--- Programmers/jecobe-js/day07.js
+++ Programmers/jecobe-js/day07.js
@@ -15,3 +15,32 @@ function solution(score) {
         );
     });
 }
+
+
+// 저주의 숫자 3
+// 3x 마을 사람들은 3을 저주의 숫자라고 생각하기 때문에 3의 배수와 숫자 3을 사용하지 않습니다. 
+// 3x 마을 사람들의 숫자는 다음과 같습니다. ( 표 생략 )
+// 정수 n이 매개변수로 주어질 때, n을 3x 마을에서 사용하는 숫자로 바꿔 return하도록 solution 함수를 완성해주세요.
+function solution(n) {
+    let ans = 0;
+    
+    for(let i = 1; i <= n; i++){
+        ans += 1;
+        
+        while(true){
+            if(ans % 3 === 0 || String(ans).includes("3")){
+                ans += 1;   
+                continue;
+            }
+            break;
+        }
+    }
+    return ans;
+}
+// 빠른 풀이
+function solution(n) {
+    let arr = [];
+    let num = 0;
+    while (arr.length !== n && ++num) if (num%3!==0 && !(''+num).includes('3')) arr.push(num);
+    return arr.pop();
+}
