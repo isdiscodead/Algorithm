345d71f4e08c0c53300153ce555c626863ac1c09
Jiwon <58460386+isdiscodead@users.noreply.github.com>
2023-10-26 15:31:46 +0900

    # 2480 주사위 세개

--- BOJ_step/03_2_if_clause_new.py
+++ BOJ_step/03_2_if_clause_new.py
@@ -14,3 +14,15 @@ print(hour, minute)
 
 
 # 2480 주사위 세개
+a, b, c = list(map(int, input().split()))
+
+if ( a == b == c ) :
+    print(10000+a*1000)
+elif (a == b):
+    print(1000+a*100)
+elif (b == c):
+    print(1000+b*100)
+elif (c == a):
+    print(1000+a*100)
+else:
+    print(max(a, b, c)*100)
